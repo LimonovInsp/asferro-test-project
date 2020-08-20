df1c10a2a0d934335c45c06680b2062ede2f81c3
LimonovInsp <bis.limonov@gmail.com>
2020-08-20 16:29:08 +0300

    Updates

--- client/src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import App from './App.vue';
-
-// eslint-disable-next-line no-new
-new Vue({
-  el: '#app',
-  // eslint-disable-next-line arrow-parens
-  render: h => h(App),
-});
